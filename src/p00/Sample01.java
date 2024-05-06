63fa6ff4760c4e85e26d62df1acb32801c558389
itlaboj <itlabo.arakawa@gmail.com>
2024-05-06 12:17:47 +0900

    初めてのJDBCプログラミング

---
+++ src/p00/Sample01.java
@@ -0,0 +1,57 @@
+package p00;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Sample01 {
+
+	public static void main(String[] args) {
+		try {
+			//JDBCドライバの登録
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("ドライバが登録されていません");
+			e.printStackTrace();
+		}
+		
+		//データベース接続情報
+		String url = "jdbc:postgresql:データベース名";
+		String user = "ユーザ名";
+		String pass = "パスワード";
+		
+		//実行のSQL文
+		String sql = "SELECT id, name FROM Members";		
+		
+		try {
+			//データベースへの接続
+			Connection con = DriverManager.getConnection(url, user, pass);
+			
+			//SQL実行の準備
+			PreparedStatement ps = con.prepareStatement(sql);
+			
+			//SQL実行結果の取得
+			ResultSet rs = ps.executeQuery();
+			
+			//結果の表示
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				
+				System.out.println(id + "\t" + name);
+			}
+			
+			//リソースのクローズ
+			rs.close();
+			ps.close();
+			con.close();
+		} catch (SQLException e) {
+			System.out.println("データベース関連エラーです");
+			e.printStackTrace();
+		}
+		
+	}
+
+}
